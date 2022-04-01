548e8c28849a84fa4a0af98d397a1d2c5d618d06
Karolina Tech Dev <101413311+KarolinaTechDev@users.noreply.github.com>
2022-04-01 12:12:49 -0400

    Create funcMul2Num.js

---
+++ funcMul2Num.js
@@ -0,0 +1,10 @@
+
+//Implement a function which multiplies two numbers.
+
+// Write here your multiply-function
+//const multiply = (num1, num2) => {
+  //return num1 * num2;
+//};
+
+let resulMultiply = multiply(4, 5);
+console.log(resulMultiply);
